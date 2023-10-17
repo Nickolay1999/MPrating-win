47c89b19c64bbca46919d7ce35e54d79fe62ccc1
Nickolay1999 <nikola.gundyrev@mail.ru>
2023-10-17 12:45:03 +0300

    1.13.29

--- bulikakam.bat
+++ bulikakam.bat
@@ -1,5 +1,5 @@
 @echo off
 taskkill /f /t /im javaw.exe
 taskkill /f /t /im java.exe
-java -noverify -Dfile.encoding=UTF-8 -jar MPrating.jar 2> last.txt
+java -noverify -Dprism.order=sw -Dfile.encoding=UTF-8 -jar MPrating.jar 2> last.txt
 type last.txt>>log.txt
